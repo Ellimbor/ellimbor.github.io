1d53c4e13695d933ffcfc5f7dde0e5c0bce64753
Ellimbor <vfuicagonzalez@gmail.com>
2024-09-26 23:14:11 -0300

    index

---
+++ NPC.js
@@ -0,0 +1,13 @@
+//Class build
+public class NPC {
+  constructor(x_coordinate,y_coordinate,name,subarea_name,area_name){
+    this.
+    this.x_coordinate = x_coordinate;
+    this.y_coordinate = y_coordinate;
+    this.name = name;
+  }
+};
+
+//Class populate
+const Mr_Mordaut_1 = new NPC(1,1,"Mr Mordaut","Anachronia Base Camp","Anachronia");
+const Laniakea_1 = new NPC(2,4,"Laniakea","Anachronia Base Camp","Anachronia"");
